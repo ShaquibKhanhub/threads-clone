d6f40d1e6ece6859b5f49a5a7f0f451bd7fe4b0d
Shaquibkhanhub <shaquibkhanrdx34@gmail.com>
2025-01-18 16:44:15 +0530

    change url

--- backend/middlewares/protectRoute.js
+++ backend/middlewares/protectRoute.js
@@ -6,6 +6,7 @@ const protectRoute = async (req, res, next) => {
     const token = req.cookies.jwt;
 
     if (!token) return res.status(401).json({ message: "Unauthorized" });
+   
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
