2421a325189e92bff2239df841ad3e579b2f5d3a
Shaquibkhanhub <shaquibkhanrdx34@gmail.com>
2025-01-18 17:41:36 +0530

    fixing bug

--- backend/middlewares/protectRoute.js
+++ backend/middlewares/protectRoute.js
@@ -2,17 +2,21 @@ import User from "../models/UserModel.js";
 import jwt from "jsonwebtoken";
 
 const protectRoute = async (req, res, next) => {
+  const token = req.cookies.jwt;
+  if (!token)
+    return res
+      .status(401)
+      .json({ success: false, message: "Unauthorized - no token provided" });
   try {
-    const token = req.cookies.jwt;
-
-    if (!token) return res.status(401).json({ message: "Unauthorized" });
-   
-
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    const user = await User.findById(decoded.userId).select("-password");
-    //req.user is just an object
-    req.user = user;
+    if (!decoded)
+      return res.status(401).json({
+        success: false,
+        message: "Unauthorized - invalid token provided",
+      });
+
+    req.userId = decoded.userId; //here we're creating a new property in req object and storing userId in it
     next();
   } catch (err) {
     res.status(500).json({ message: err.message });
